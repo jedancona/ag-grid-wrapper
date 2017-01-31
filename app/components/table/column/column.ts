ba5fe29218029fe00229ec6e141a17bb043aa5de
jedancona <jed.ancona@cse-icon.com>
2017-01-31 10:14:08 -0600

    Row auto save (#9)
    * Setup Framework for row auto save.
    
    * Auto Row Save work in progress.
    
    * Minor modifcations to binding and added type on remove row.

--- app/components/table/column/column.ts
+++ app/components/table/column/column.ts
@@ -1,7 +1,8 @@
-import {Component, ViewEncapsulation, Input} from "@angular/core";
+import {Component, ViewEncapsulation, Input, AfterViewInit} from "@angular/core";
 import {GridApi, ColumnApi, GridParams} from "ag-grid/main";
 import {TableComponent} from "../table";
-import * as _ from 'lodash';
+import * as _ from "lodash";
+import {NumericEditorComponent} from "../editors/numeric-editor/numeric-editor";
 
 @Component({
   selector: 'ui-table-column',
@@ -12,31 +13,30 @@ import * as _ from 'lodash';
 
 export class TableColumnComponent {
 
-
-
   private gridParams: GridParams;
 
   private colDef: any = {};
   // making these public, so they are accessible to people using the ng2 component references
   public api: GridApi;
   public columnApi: ColumnApi;
+  public cellEditorFramework: any;
 
   constructor(private table: TableComponent) {
-
-    this.suppressMovable = true;
-    _.defaults(this,{
+    _.defaults(this, {
       minWidth: 50,
       suppressMovable: true,
     });
+    this.cellEditorFramework =  NumericEditorComponent;
     this.table.addColumn(this);
   }
 
-
   @Input('display-name') public headerName: string = undefined;
   @Input('enableMovable') public suppressMovable: boolean = false;
+  @Input('type') public type: string = undefined;
   @Input('name') public field: any = undefined;
   @Input('enable-edit') public editable: boolean = true;
   @Input('cell-editor') public cellEditor: string = undefined;
   @Input('minWidth') public minWidth: number = undefined;
+
 
 }
---
+++ app/components/table/editors/numeric-editor/numeric-editor.ts
@@ -0,0 +1,63 @@
+import {Component, ViewContainerRef, ViewChild, AfterViewInit} from '@angular/core';
+
+import {AgEditorComponent} from 'ag-grid-ng2/main';
+
+@Component({
+  selector: 'numeric-cell',
+  template: `<input #input (keydown)="onKeyDown($event)" (blur)="onBlur($event)" [(ngModel)]="value">`
+})
+export class NumericEditorComponent implements AgEditorComponent, AfterViewInit {
+  private params: any;
+  public value: number;
+  private cancelBeforeStart: boolean = false;
+
+  @ViewChild('input', {read: ViewContainerRef}) public input: any;
+
+
+  agInit(params: any): void {
+    this.params = params;
+    this.value = this.params.value;
+    console.debug('numeric editor');
+    // only start edit if key pressed is a number, not a letter
+    this.cancelBeforeStart = params.charPress && ('1234567890'.indexOf(params.charPress) < 0);
+  }
+
+  getValue(): any {
+    return this.value;
+  }
+
+  isCancelBeforeStart(): boolean {
+    return this.cancelBeforeStart;
+  }
+
+  onKeyDown(event: any): void {
+    if (!this.isKeyPressedNumeric(event)) {
+      if (event.preventDefault) event.preventDefault();
+    }
+  }
+
+  onBlur(event: any): void {
+    console.debug(event);
+    this.params.api.stopEditing();
+  }
+
+  // dont use afterGuiAttached for post gui events - hook into ngAfterViewInit instead for this
+  ngAfterViewInit() {
+    this.input.element.nativeElement.focus();
+  }
+
+  private getCharCodeFromEvent(event: any): any {
+    event = event || window.event;
+    return (typeof event.which == "undefined") ? event.keyCode : event.which;
+  }
+
+  private isCharNumeric(charStr: any): boolean {
+    return !!/\d/.test(charStr);
+  }
+
+  private isKeyPressedNumeric(event: any): boolean {
+    var charCode = this.getCharCodeFromEvent(event);
+    var charStr = String.fromCharCode(charCode);
+    return this.isCharNumeric(charStr);
+  }
+}
