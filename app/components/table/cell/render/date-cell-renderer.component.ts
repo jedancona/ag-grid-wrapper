ab6285eb374789afa02763485eed7dc4be775999
Jeremy Odell <jeremyodell@gmail.com>
2017-02-02 12:33:07 -0600

    'cell'

---
+++ app/components/table/cell/render/date-cell-renderer.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+import { AgRendererComponent } from "ag-grid-ng2";
+@Component({
+  selector: 'date-cell',
+  template: '{{dateValue | date:DEFAULT_DATE_FORMAT}}'
+})
+export class DateCellRendererComponent implements AgRendererComponent {
+  private DEFAULT_DATE_FORMAT = 'MM/dd/y';
+  private params: any;
+  private dateValue: string;
+  private filter: string;
+  agInit(params: any): void {
+    this.params = params;
+    this.dateValue = this.params.value;
+    this.filter = this.params.colDef.cellFilter;
+    if(this.filter){
+      this.filter = this.filter.substring(this.filter.indexOf(':')+1);
+      this.DEFAULT_DATE_FORMAT = this.filter;
+    }
+  }
+
+  refresh(params: any): void {
+  }
+
+
+}
