836d2e856fef843cb5304a75a4fd030d546acc0e
Jeremy Odell <jeremyodell@gmail.com>
2017-02-09 14:18:12 -0600

    package changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./app'));
-__export(require('./app/components'));
-__export(require('./app/components/components.module'));
-__export(require('./app/components/table'));
+__export(require("./app"));
+__export(require("./app/components"));
+__export(require("./app/components/components.module"));
+__export(require("./app/components/table"));
 //# sourceMappingURL=index.js.map
