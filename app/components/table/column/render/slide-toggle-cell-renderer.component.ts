266e686ee68528cf1a3e601fd8a96912b7159293
Jed Ancona <jed.ancona@energytransfer.com>
2017-02-15 09:42:44 -0600

    Update the checkbox and slider toggle components to set the dirty status on the rows.

--- app/components/table/column/render/slide-toggle-cell-renderer.component.ts
+++ app/components/table/column/render/slide-toggle-cell-renderer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { AgRendererComponent } from 'ag-grid-ng2';
 @Component({
   selector: 'slide-toggle-cell',
-  template: '<md-slide-toggle (change)="sliderChanged($event)" [checked]="isChecked()" [disabled]="isEditable()" aria-label="slide" ></md-slide-toggle>'
+  template: '<md-slide-toggle (change)="valueChanged($event)" [checked]="isChecked()" [disabled]="isEditable()" aria-label="slide" ></md-slide-toggle>'
 })
 export class SlideToggleCellRendererComponent implements AgRendererComponent {
   protected params: any;
@@ -15,7 +15,7 @@ export class SlideToggleCellRendererComponent implements AgRendererComponent {
     this.modelType = this.params.colDef.modelType;
   }
 
-  public sliderChanged = ($event: any): void => {
+  public valueChanged = ($event: any): void => {
     switch (this.modelType) {
       case 'yesno':
         this.setYesNoModelValue($event.checked);
@@ -26,6 +26,9 @@ export class SlideToggleCellRendererComponent implements AgRendererComponent {
       default:
         this.setTrueFalseModelValue($event.checked);
     }
+    let gridRow: any = this.params.node;
+    this.params.api.rowEdit.setGridRowDirty(this.params.api, gridRow);
+    this.params.api.refreshRows([ gridRow ]);
   };
 
   public isChecked = (): boolean => {
